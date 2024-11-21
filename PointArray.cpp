a45e9720a9cb92a436f22dd7e5e8a9e02e07058f
Bradyxavier25 <xavierbrady18@gmail.com>
2024-11-21 06:39:51 -0500

    Update PointArray.cpp

--- PointArray.cpp
+++ PointArray.cpp
@@ -4,19 +4,19 @@ using namespace std;
 
 class PointArray {
 private:
-    Punto* points; // Puntero dinámico a un arreglo de puntos.
-    int size;      // Tamaño del arreglo.
+    Punto* points; 
+    int size;      
 
 public:
-    // Constructor por defecto.
+    
     PointArray() : points(nullptr), size(0) {}
 
-    // Constructor con tamaño inicial.
+    
     PointArray(int size) : size(size) {
         points = new Punto[size];
     }
 
-    // Constructor de copia.
+    
     PointArray(const PointArray& other) : size(other.size) {
         points = new Punto[size];
         for (int i = 0; i < size; ++i) {
@@ -24,15 +24,15 @@ public:
         }
     }
 
-    // Destructor.
+    
     ~PointArray() {
         delete[] points;
     }
 
-    // Sobrecarga del operador de asignación.
+    
     PointArray& operator=(const PointArray& other) {
-        if (this != &other) { // Evitar autoasignación.
-            delete[] points; // Liberar memoria actual.
+        if (this != &other) { 
+            delete[] points; 
 
             size = other.size;
             points = new Punto[size];
@@ -43,7 +43,7 @@ public:
         return *this;
     }
 
-    // Sobrecarga del operador [] para lectura/escritura.
+    
     Punto& operator[](int index) {
         return points[index];
     }
@@ -52,12 +52,12 @@ public:
         return points[index];
     }
 
-    // Obtener el tamaño del arreglo.
+    
     int getSize() const {
         return size;
     }
 
-    // Imprimir los puntos del arreglo.
+
     void display() const {
         for (int i = 0; i < size; ++i) {
             points[i].display();
